ecadbe472fe12b0eab6ecb26da5316f06f428934
George V. Reilly <george@reilly.org>
2010-07-18 08:45:59 -0700

    Stripped BookFormat lines

--- bookdata2.js
+++ bookdata2.js
@@ -5,7 +5,6 @@ Readerware.Data.Books = [
  {
   "Title": "Photoshop Lightroom 2 Adventure",
   "Author": "Aaland, Mikkel",
-  "BookFormat": "Paperback",
   "ISBN": "0-596-52101-4",
   "CopyDate": "2009"
  },
@@ -13,7 +12,6 @@ Readerware.Data.Books = [
  {
   "Title": "Assignment - Ceylon",
   "Author": "Aarons, Edward S.",
-  "BookFormat": "Paperback",
   "ISBN": "",
   "CopyDate": "1973"
  },
@@ -21,7 +19,6 @@ Readerware.Data.Books = [
  {
   "Title": "Assignment - Nuclear Nude",
   "Author": "Aarons, Edward S.",
-  "BookFormat": "Paperback",
   "ISBN": "",
   "CopyDate": "1968"
  },
@@ -29,7 +26,6 @@ Readerware.Data.Books = [
  {
   "Title": "Fallen Heroes (ST:DS9 # 5)",
   "Author": "ab Hugh, Dafydd",
-  "BookFormat": "Mass Market Paperback",
   "ISBN": "0-671-88459-X",
   "CopyDate": "1994"
  },
@@ -37,7 +33,6 @@ Readerware.Data.Books = [
  {
   "Title": "Bahamas Crisis",
   "Author": "Bagley, Desmond",
-  "BookFormat": "Paperback",
   "ISBN": "0-06-080755-5",
   "CopyDate": "1980"
  },
@@ -45,7 +40,6 @@ Readerware.Data.Books = [
  {
   "Title": "The Golden Keel",
   "Author": "Bagley, Desmond",
-  "BookFormat": "Trade Paperback",
   "ISBN": "0-06-080730-X",
   "CopyDate": "1963"
  },
@@ -53,7 +47,6 @@ Readerware.Data.Books = [
  {
   "Title": "The Vivero Letter",
   "Author": "Bagley, Desmond",
-  "BookFormat": "Paperback",
   "ISBN": "0-00-615397-6",
   "CopyDate": "1968"
  },
@@ -61,7 +54,6 @@ Readerware.Data.Books = [
  {
   "Title": "Wyatt's Hurincane",
   "Author": "Bagley, Desmond",
-  "BookFormat": "Mass Market Paperback",
   "ISBN": "0-00-612830-0",
   "CopyDate": "1966"
  },
@@ -69,7 +61,6 @@ Readerware.Data.Books = [
  {
   "Title": "Cut the Clutter and Stow the Stuff : The Q.U.I.C.K. Way to Bring Lasting Order to Household Chaos",
   "Author": "Baird, Lori (Editor)",
-  "BookFormat": "Paperback",
   "ISBN": "1-57954-512-2",
   "CopyDate": "2002"
  },
@@ -77,7 +68,6 @@ Readerware.Data.Books = [
  {
   "Title": "Finishes in the Ethnic Tradition",
   "Author": "Baizerman, Suzanne & Karen Searle",
-  "BookFormat": "Paperback",
   "ISBN": "0-932394-01-9",
   "CopyDate": "1978"
  },
@@ -85,7 +75,6 @@ Readerware.Data.Books = [
  {
   "Title": "The Complete Potato Cookbook",
   "Author": "Bakalar, Ruth",
-  "BookFormat": "Hardcover",
   "ISBN": "",
   "CopyDate": "1969"
  },
@@ -93,7 +82,6 @@ Readerware.Data.Books = [
  {
   "Title": "The Rome Affair",
   "Author": "Caldwell, Laura",
-  "BookFormat": "Mass Market Paperback",
   "ISBN": "0-7783-2309-9",
   "CopyDate": "2006"
  },
@@ -101,7 +89,6 @@ Readerware.Data.Books = [
  {
   "Title": "Doc and Fluff: The Distopian Tale of a Girl and Her Biker",
   "Author": "Califia, Pat",
-  "BookFormat": "Paperback",
   "ISBN": "1-55583-176-1",
   "CopyDate": "1990"
  },
@@ -109,7 +96,6 @@ Readerware.Data.Books = [
  {
   "Title": "Macho Sluts",
   "Author": "Califia, Pat",
-  "BookFormat": "Paperback",
   "ISBN": "1-55583-115-X",
   "CopyDate": "1988"
  },
@@ -117,7 +103,6 @@ Readerware.Data.Books = [
  {
   "Title": "Sapphistry : The Book of Lesbian Sexuality",
   "Author": "Califia, Pat",
-  "BookFormat": "Trade Paperback",
   "ISBN": "0-941483-24-X",
   "CopyDate": "1988"
  },
@@ -125,7 +110,6 @@ Readerware.Data.Books = [
  {
   "Title": "The Advocate Adviser: America's Most Popular Gay Columnist Tackles the Questions That the Others Ignore",
   "Author": "Califia, Pat",
-  "BookFormat": "Paperback",
   "ISBN": "1-55583-169-9",
   "CopyDate": "1991"
  },
@@ -133,7 +117,6 @@ Readerware.Data.Books = [
  {
   "Title": "The Best of Irish Breads and Baking",
   "Author": "Campbell, Georgina",
-  "BookFormat": "Paperback",
   "ISBN": "0-86327-500-1",
   "CopyDate": "1996"
  },
@@ -141,7 +124,6 @@ Readerware.Data.Books = [
  {
   "Title": "Myths to Live By",
   "Author": "Campbell, Joseph",
-  "BookFormat": "Paperback",
   "ISBN": "0-553-27088-5",
   "CopyDate": "1972"
  },
@@ -149,7 +131,6 @@ Readerware.Data.Books = [
  {
   "Title": "Pact of the Fathers",
   "Author": "Campbell, Ramsey",
-  "BookFormat": "Hardcover",
   "ISBN": "0-312-87869-9",
   "CopyDate": "2001"
  },
@@ -157,7 +138,6 @@ Readerware.Data.Books = [
  {
   "Title": "\"Horizon\" at the Frontiers of Medicine",
   "Author": "Campbell-Jones, Simon",
-  "BookFormat": "Paperback",
   "ISBN": "0-563-17955-4",
   "CopyDate": "1983"
  },
@@ -165,7 +145,6 @@ Readerware.Data.Books = [
  {
   "Title": "Photo Finish: The Digital Photographer's Guide to Printing, Showing, and Selling Images",
   "Author": "Canfield, Jon & Tim Grey",
-  "BookFormat": "Oversize Paperback",
   "ISBN": "0-7821-4348-2",
   "CopyDate": "2004"
  },
@@ -173,7 +152,6 @@ Readerware.Data.Books = [
  {
   "Title": "The Thin Woman",
   "Author": "Cannell, Dorothy",
-  "BookFormat": "Paperback",
   "ISBN": "0-14-007947-5",
   "CopyDate": "1984"
  },
@@ -181,7 +159,6 @@ Readerware.Data.Books = [
  {
   "Title": "The Widows Club",
   "Author": "Cannell, Dorothy",
-  "BookFormat": "Mass Market Paperback",
   "ISBN": "0-553-27794-4",
   "CopyDate": "1988"
  },
@@ -189,7 +166,6 @@ Readerware.Data.Books = [
  {
   "Title": "The Civilization of the Middle Ages: A completely revised and expanded edition of Medieval history, the life and death of a civilization",
   "Author": "Cantor, Norman F",
-  "BookFormat": "Hardcover",
   "ISBN": "0-06-017033-6",
   "CopyDate": "1993"
  },
@@ -197,7 +173,6 @@ Readerware.Data.Books = [
  {
   "Title": "Inventing the Middle Ages",
   "Author": "Cantor, Norman F.",
-  "BookFormat": "Paperback",
   "ISBN": "0-688-12302-3",
   "CopyDate": "1991"
  },
@@ -205,7 +180,6 @@ Readerware.Data.Books = [
  {
   "Title": "The Pimlico Encyclopedia of the Middle Ages",
   "Author": "Cantor, Norman F.",
-  "BookFormat": "Paperback",
   "ISBN": "0-7126-6407-6",
   "CopyDate": "1999"
  },
@@ -213,7 +187,6 @@ Readerware.Data.Books = [
  {
   "Title": "Color: A Course in Mastering the Art of Mixing Colors",
   "Author": "Edwards, Betty",
-  "BookFormat": "Paperback",
   "ISBN": "1-58542-219-3",
   "CopyDate": "2004"
  },
@@ -221,7 +194,6 @@ Readerware.Data.Books = [
  {
   "Title": "The New Drawing on the Right Side of the Brain",
   "Author": "Edwards, Betty",
-  "BookFormat": "Paperback",
   "ISBN": "0-87477-424-1",
   "CopyDate": "1999"
  },
@@ -229,7 +201,6 @@ Readerware.Data.Books = [
  {
   "Title": "The New Drawing on the Right Side of the Brain Workbook:  Guided Practice in the Five Basic Skills of Drawing",
   "Author": "Edwards, Betty",
-  "BookFormat": "Spiral-bound",
   "ISBN": "1-58542-195-2",
   "CopyDate": "2002"
  },
@@ -237,7 +208,6 @@ Readerware.Data.Books = [
  {
   "Title": "Working from Home (4th edition)",
   "Author": "Edwards, Paul",
-  "BookFormat": "Paperback",
   "ISBN": "0-87477-764-X",
   "CopyDate": "1994"
  },
@@ -245,7 +215,6 @@ Readerware.Data.Books = [
  {
   "Title": "Matricide at St Martha's",
   "Author": "Edwards, Ruth Dudley",
-  "BookFormat": "Hardcover",
   "ISBN": "0-312-13122-4",
   "CopyDate": "1994"
  },
@@ -253,7 +222,6 @@ Readerware.Data.Books = [
  {
   "Title": "Publish and Be Murdered",
   "Author": "Edwards, Ruth Dudley",
-  "BookFormat": "Paperback",
   "ISBN": "0-00-649863-9",
   "CopyDate": "1998"
  },
@@ -261,7 +229,6 @@ Readerware.Data.Books = [
  {
   "Title": "Colourful Cushions in Needlepoint: 20 Needlepoint Projects",
   "Author": "Edwards, Stella",
-  "BookFormat": "Hardcover",
   "ISBN": "0-7134-7996-5",
   "CopyDate": "1997"
  },
@@ -269,7 +236,6 @@ Readerware.Data.Books = [
  {
   "Title": "When Gravity Fails",
   "Author": "Effinger, George",
-  "BookFormat": "Paperback",
   "ISBN": "0-553-25555-X",
   "CopyDate": "1987"
  },
@@ -277,7 +243,6 @@ Readerware.Data.Books = [
  {
   "Title": "James Joyce A to Z : The Essential Reference to His Life and Writings  (Literary a to Z's)",
   "Author": "Fargnoli, A. Nicholas & Gillespie, Michael",
-  "BookFormat": "Paperback",
   "ISBN": "0-19-511029-3",
   "CopyDate": "1995"
  },
@@ -285,7 +250,6 @@ Readerware.Data.Books = [
  {
   "Title": "Star Trek: A Flag Full of Stars",
   "Author": "Ferguson, Brad",
-  "BookFormat": "Mass Market Paperback",
   "ISBN": "0-671-73918-2",
   "CopyDate": "1991"
  },
@@ -293,7 +257,6 @@ Readerware.Data.Books = [
  {
   "Title": "Practical Cryptography",
   "Author": "Ferguson, Niels & Schneier, Bruce",
-  "BookFormat": "Paperback",
   "ISBN": "0-471-22357-3",
   "CopyDate": "2003"
  },
@@ -301,7 +264,6 @@ Readerware.Data.Books = [
  {
   "Title": "The Vikings: A History",
   "Author": "Ferguson, Robert",
-  "BookFormat": "Hardcover",
   "ISBN": "0-670-02079-6",
   "CopyDate": "2009"
  },
@@ -309,7 +271,6 @@ Readerware.Data.Books = [
  {
   "Title": "The Grantville Gazette",
   "Author": "Flint, Eric (Editor)",
-  "BookFormat": "Paperback",
   "ISBN": "0-7434-8860-1",
   "CopyDate": "2004"
  },
@@ -317,7 +278,6 @@ Readerware.Data.Books = [
  {
   "Title": "1634: The Galileo Affair",
   "Author": "Flint, Eric and Dennis, Andrew",
-  "BookFormat": "Paperback",
   "ISBN": "0-7434-9919-0",
   "CopyDate": "2004"
  },
@@ -325,7 +285,6 @@ Readerware.Data.Books = [
  {
   "Title": "1633",
   "Author": "Flint, Eric and Weber, David",
-  "BookFormat": "Mass Market Paperback",
   "ISBN": "0-7434-7155-5",
   "CopyDate": "2003"
  },
