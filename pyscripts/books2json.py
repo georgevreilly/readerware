93f82e490335320615604e010125a91eb5873486
George V. Reilly <george@reilly.org>
2010-07-22 02:36:00 -0400

    split_first_last_name

--- pyscripts/books2json.py
+++ pyscripts/books2json.py
@@ -5,8 +5,12 @@ import re
 
 re_conjunction = re.compile(r"[&:;]|\Wand\W", re.IGNORECASE)
 
-def parse_author(author_entry):
-    return author_entry
+def split_first_last_name(author_entry):
+    names = [name.strip() for name in author_entry.split(",")]
+    if len(names) == 1:
+        names = [name.strip() for name in author_entry.split()]
+        names = [names[-1], ' '.join(names[:-1])]
+    return names
 
 def split_multiple_authors(authors_entry):
     return [au.strip() for au in re_conjunction.split(authors_entry)]
@@ -14,6 +18,7 @@ def split_multiple_authors(authors_entry):
 def parse_csv(csv_filename):
     bookReader = csv.DictReader(open(csv_filename))
     items = [x for x in bookReader]
+    # TODO: handle &doublequote;
     print len(items), bookReader.fieldnames
     
 if __name__ == '__main__':
@@ -22,8 +27,15 @@ if __name__ == '__main__':
     import unittest
 
     class TestParsing(unittest.TestCase):
-        def test_parse_author(self):
-            self.assertEqual([('Joyce', 'James')], parse_author('Joyce, James'))
+        def test_split_first_last_name(self):
+            self.assertEqual(['Joyce', 'James'], split_first_last_name('Joyce, James'))
+            self.assertEqual(["Moulitsas Zuniga", "Markos"], split_first_last_name("Moulitsas Zuniga, Markos"))
+            self.assertEqual(["di Montezemolo", "Catherine"], split_first_last_name("di Montezemolo, Catherine"))
+            self.assertEqual(["Rossi", "Jean Baptiste"], split_first_last_name("Rossi, Jean Baptiste"))
+            self.assertEqual(["van Dam", "Andries"], split_first_last_name("van Dam, Andries"))
+            self.assertEqual(["Aho", "Alfred V."], split_first_last_name("Aho, Alfred V."))
+            self.assertEqual(["Elrod", "P. N."], split_first_last_name("P. N. Elrod"))
+            self.assertEqual(["Woodham-Smith", "Cecil"], split_first_last_name("Cecil Woodham-Smith"))
 
         def test_split_multiple_authors(self):
             self.assertEqual(
@@ -53,7 +65,9 @@ if __name__ == '__main__':
             self.assertEqual(
                 ["Foley, James D.", "van Dam, Andries", "Feiner, Steven K.", "Hughes, John F."],
                 split_multiple_authors("Foley, James D.: van Dam, Andries; Feiner, Steven K.; Hughes, John F."))
-
+# "Hamilton, Laurell K. and MaryJanice Davidson, Eileen Wilks, Rebecca York"
+# "Roghaar, Linda (Editor) and Wolf, Nancy"
+# "Gies, Joseph and Francis"
 
     parser = OptionParser()
     parser.add_option("-t", "--test", action="store_true", dest="test",
